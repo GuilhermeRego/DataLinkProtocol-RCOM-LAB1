56a1f6f59502b09972097e1962aedc0dbd143aa5
Guilherme Rego <up202207041@fe.up.pt>
2024-10-21 20:53:48 +0100

    llopen, llwrite

--- src/application_layer.c
+++ src/application_layer.c
@@ -1,22 +1,206 @@
-// Application layer protocol implementation
-
 #include "application_layer.h"
-
+#include "link_layer.h"
+#include <string.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <math.h>
 
-void applicationLayer(const char *serialPort, const char *role, int baudRate,
-                      int nTries, int timeout, const char *filename)
-{
-    LinkLayer linklayer;
-    strcpy(linklayer.serialPort, serialPort);
-    linklayer.baudRate = baudRate;
-    linklayer.nRetransmissions = nTries;
-    linklayer.timeout = timeout;
-    linklayer.role = strcmp(role, "tx") == 0 ? LlTx : LlRx;
+// Função que interpreta ("parseia") um frame de controle
+unsigned char* parseControlFrame(unsigned char* frame, int size, unsigned long int *fileSize) {
 
-    int fd = llopen(linklayer);
+    // Extrai o tamanho do arquivo (File Size)
+    unsigned char fileSize1 = frame[2];  // Pega o tamanho do campo de tamanho do arquivo
+    unsigned char fileSize2[fileSize1];  // Cria um array temporário para armazenar o tamanho do arquivo
+    memcpy(fileSize2, frame+3, fileSize1); // Copia o tamanho do arquivo do quadro para o array temporário
+
+    // Converte os bytes para formar o valor completo do tamanho do arquivo
+    for(int i = 0; i < fileSize1; i++) {
+        *fileSize |= (fileSize2[fileSize1 - i - 1] << (i * 8)); // Constrói o tamanho do arquivo somando os bytes
+    }
+
+    // Extrai o nome do arquivo (File Name)
+    unsigned char fileNBytes = frame[3 + fileSize1 + 1]; // Tamanho do nome do arquivo
+    unsigned char *ret = (unsigned char*)malloc(fileNBytes); // Aloca memória para o nome do arquivo
+    memcpy(ret, frame + 3 + fileSize1 + 2, fileNBytes); // Copia o nome do arquivo para o buffer retornado
+
+    return ret; // Retorna o nome do arquivo
+}
+
+// Função para construir um quadro de controle (control frame)
+unsigned char* getControlFrame(const unsigned int c, const char* filename, long int len, unsigned int* size) {
+    // Calcula o tamanho necessário para armazenar o tamanho do arquivo (em bytes)
+    int L0 = (int) ((float) len / 8.0); // Tamanho do arquivo em bytes 
+    int L1 = strlen(filename); // Tamanho do nome do arquivo
+    *size = L0 + L1 + 5; // O tamanho total do quadro
+
+    // Aloca memória para o quadro de controle
+    unsigned char *frame = (unsigned char*) malloc(*size);
+    
+    unsigned int pos = 0;
+    frame[pos++] = c;   // Campo de controle (c: START = 1, END = 3)
+    frame[pos++] = 0;   // Campo indicador de tamanho do arquivo (ID)
+    frame[pos++] = L0;  // Quantos bytes o campo de tamanho do arquivo tem (L0)
+
+    // Converte o tamanho do arquivo para uma sequência de bytes
+    for (unsigned char i = 0 ; i < L0 ; i++) {
+        frame[L0 - i + 2] = len & 0xFF; // Pega os últimos 8 bits de "len"
+        len >>= 8; // Desloca o tamanho do arquivo para processar o próximo byte
+    }
+    
+    pos += L0; // Atualiza o ponteiro
+    frame[pos++] = 1;   // ID para o nome do arquivo
+    frame[pos++] = L1;  // Quantos bytes o nome do arquivo tem
+    memcpy(frame + pos, filename, L1);  // Copia o nome do arquivo no quadro de controle
+
+    return frame; // Retorna o quadro de controle
+}
+
+// Função que cria e retorna um frame de dados (data frame)
+unsigned char * getDataFrame(unsigned char sequence, unsigned char *data, int dataSize, int *frameSize) {
+    // O frame de dados é composto por 5 bytes de cabeçalho e o conteúdo dos dados
+    *frameSize = 5 + dataSize;
+    unsigned char* frame = (unsigned char*) malloc(*frameSize);
+
+    // Preenche o cabeçalho do quadro de dados
+    frame[0] = 1;  // Tipo de frame: 1 para frame de dados
+    frame[1] = sequence;  // Número de sequência (para controle de duplicatas e ordem)
+    frame[2] = (dataSize >> 8) & 0xFF;  // Parte alta do tamanho dos dados
+    frame[3] = dataSize & 0xFF;  // Parte baixa do tamanho dos dados
+    memcpy(frame + 4, data, dataSize);  // Copia os dados reais para o frame
+
+    return frame;  // Retorna o quadro de dados
+}
+
+// Função que lê o conteúdo do arquivo e o coloca em um buffer
+unsigned char * getData(FILE* fd, long int fileLength) {
+    // Aloca memória para o conteúdo do arquivo
+    unsigned char* content = (unsigned char*)malloc(sizeof(unsigned char) * fileLength);
+
+    // Lê o arquivo completo para a memória
+    fread(content, sizeof(unsigned char), fileLength, fd);
+
+    return content;  // Retorna o buffer de dados do arquivo
+}
+
+// Função que interpreta um quadro de dados e copia os dados para um buffer
+void parseDataframe(const unsigned char* frame, const unsigned int frameSize, unsigned char* buffer) {
+    // Copia os dados do quadro (ignorando o cabeçalho de 4 bytes)
+    memcpy(buffer, frame + 4, frameSize - 4);
+    buffer += frameSize - 4; // Atualiza o ponteiro do buffer
+}
+
+// Função principal da camada de aplicação
+void applicationLayer(const char *serialPort, const char *role, int baudRate, int nTries, int timeout, const char *filename) {
+    // Completa a estrutura de dados do link layer
+    LinkLayer linkLayer;
+    strcpy(linkLayer.serialPort, serialPort);
+    linkLayer.role = strcmp(role, "tx") ? LlRx : LlTx;
+    linkLayer.baudRate = baudRate;
+    linkLayer.nRetransmissions = nTries;
+    linkLayer.timeout = timeout;
+
+    // Abre a conexão com o link layer
+    int fd = llopen(linkLayer);
     if (fd < 0) {
-        printf("ERROR: Failed to open connection\n");
+        perror("Connection error\n");
         exit(-1);
+    }
+
+    // Verifica se o papel é transmissor (LlTx) ou receptor (LlRx)
+    switch (linkLayer.role) {
+        case LlTx: {  // Transmissor
+            // Abre o arquivo para leitura
+            FILE* file = fopen(filename, "r");
+            if (file == NULL) {
+                perror("File not found\n");
+                exit(-1);
+            }
+            
+            // Calcula o tamanho do arquivo
+            int prev = ftell(file);
+            fseek(file, 0, SEEK_END);
+            long int fileSize = ftell(file) - prev;
+            fseek(file, prev, SEEK_SET);
+
+            // Cria e envia o frame de controle inicial (START)
+            unsigned int cfSize;
+            unsigned char *controlFrameStart = getControlFrame(2, filename, fileSize, &cfSize);
+            if(llwrite(controlFrameStart, cfSize) == -1){ 
+                printf("Exit: error in start frame\n");
+                exit(-1);
+            }
+
+            // Lê o conteúdo do arquivo e envia os frames dos dados em si
+            unsigned char sequence = 0;
+            unsigned char* content = getData(file, fileSize);
+            long int bytesLeft = fileSize;
+
+            // Envia os quadros de dados
+            while (bytesLeft > 0) {
+                int dataSize = bytesLeft > (long int) MAX_PAYLOAD_SIZE ? MAX_PAYLOAD_SIZE : bytesLeft;
+                int isLast = (dataSize != MAX_PAYLOAD_SIZE) ? 1 : 0; // Verifica se é o último frame
+
+                // Cria e envia o frames de dados
+                int frameSize;
+                unsigned char* frame = getDataFrame(sequence, content, dataSize, &frameSize);
+                if(llwrite(frame, frameSize) == -1) {
+                    printf("Exit: error in data frames\n");
+                    exit(-1);
+                }
+
+                // Atualiza as variáveis de controle
+                bytesLeft -= dataSize;
+                content += dataSize;
+                sequence++;
+                sequence %= 255;
+            }
+            
+            // Cria e envia o quadro de controle final (END)
+            unsigned char *controlFrameEnd = getControlFrame(3, filename, fileSize, &cfSize);
+            if(llwrite(controlFrameEnd, cfSize) == -1) { 
+                printf("Exit: error in end frame\n");
+                exit(-1);
+            }
+
+            // Fecha a conexão
+            llclose(fd);
+            break;
+        }
+
+        case LlRx: {  // Receptor
+            // Recebe o frame de controle inicial (START)
+            unsigned char *frame = (unsigned char *) malloc(MAX_PAYLOAD_SIZE);
+            int frameSize = 0;
+            while ((frameSize = llread(frame)) < 0);  // Espera pelo frame de controle
+
+            unsigned long int rxFileSize = 0;
+            unsigned char* name = parseControlFrame(frame, frameSize, &rxFileSize);  // Parse ao frame de controle
+
+            // Cria um novo arquivo para salvar os dados recebidos (com o nome do arquivo e permissões de escrita)
+            FILE* newFile = fopen((char *) name, "wb+");
+            while (1) {    
+                // Recebe o próximo frame de dados
+                while ((frameSize = llread(frame)) < 0);
+
+                // Se o frame for de controle END, finaliza
+                if(frameSize == 0) break;
+
+                // Caso contrário, processa o frame de dados
+                else if(frame[0] != 3){
+                    unsigned char *buffer = (unsigned char*)malloc(frameSize);
+                    parseDataframe(frame, frameSize, buffer);
+                    fwrite(buffer, sizeof(unsigned char), frameSize - 4, newFile);  // Escreve os dados no arquivo
+                    free(buffer);
+                }
+            }
+            
+            // Fecha o arquivo
+            fclose(newFile);
+            break;
+        }
     }
 }
--- src/link_layer.c
+++ src/link_layer.c
@@ -5,6 +5,33 @@
 
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
+#define ESCAPE 0x7D
+#define STUFF 0x20
+
+#define FLAG 0x7E 
+#define A_TX 0x03 // Transmitter
+#define A_RX 0x01 // Receiver
+#define C_SET 0x03 // Transmitter
+#define C_UA 0x07 // Receiver
+
+#define C_REJ(N) (0x01 | (N << 7)) // Reject
+#define C_RR(N) (0x05 | (N << 7)) // Receive Ready
+
+int timeout = 0;
+retransmissions = 0;
+
+int alarmTriggered = FALSE;
+int alarmCount = 0;
+
+unsigned char trama = 0;
+
+unsigned char controlFrameTX[5] = {FLAG, A_TX, C_SET, A_TX^C_SET, FLAG};
+unsigned char controlFrameRX[5] = {FLAG, A_RX, C_UA, A_RX^C_UA, FLAG};
+
+void alarmHandler(int signal) {
+    alarmTriggered = TRUE;
+    alarmCount++;
+}
 
 typedef enum {
     START,
@@ -18,28 +45,230 @@ typedef enum {
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
-int llopen(LinkLayer connectionParameters)
-{
+int llopen(LinkLayer connectionParameters) {
+    // Open serial port
     int fd = openSerialPort(connectionParameters.serialPort, connectionParameters.baudRate);
     if (fd < 0) {
         printf("ERROR: Failed to open serial port\n");
         return -1;
     }
-    
-    
-    
 
-    return 1;
+    // Set connection parameters
+    timeout = connectionParameters.timeout;
+    retransmissions = connectionParameters.nRetransmissions;
+    LinkLayerState state = START;
+
+    // Establish connection
+    switch (connectionParameters.role) {
+        case LlTx: // Transmitter
+            (void) signal(SIGALRM, alarmHandler); // Install alarm handler
+            // While we haven't received the frames and we still have retransmissions
+            while (state != STOP && retransmissions) {
+                if (writeBytesSerialPort(controlFrameTX, 5) < 0) { // Write control frame
+                    printf("ERROR: Failed to write control frame\n");
+                    return -1;
+                }
+                alarm(timeout);
+                alarmTriggered = FALSE;
+
+                // While we haven't received the frames and the alarm hasn't been triggered
+                while (state != STOP && !alarmTriggered) {
+                    // Read byte from serial port
+                    unsigned char byte;
+                    if (readByteSerialPort(&byte) < 0) {
+                        printf("ERROR: Failed to read byte from serial port\n");
+                        return -1;
+                    }
+                    else {
+                        // Check byte
+                        switch (state) {
+                            case START:
+                                if (byte == FLAG) {
+                                    state = FLAG_RCV;
+                                }
+                                break;
+                            case FLAG_RCV:
+                                if (byte == A_RX) {
+                                    state = A_RCV;
+                                } else if (byte != FLAG) {
+                                    state = START;
+                                }
+                                break;
+                            case A_RCV:
+                                if (byte == C_UA) {
+                                    state = C_RCV;
+                                } else if (byte == FLAG) {
+                                    state = FLAG_RCV;
+                                } else {
+                                    state = START;
+                                }
+                                break;
+                            case C_RCV:
+                                if (byte == (A_RX^C_UA)) {
+                                    state = BCC_OK;
+                                } else if (byte == FLAG) {
+                                    state = FLAG_RCV;
+                                } else {
+                                    state = START;
+                                }
+                                break;
+                            case BCC_OK:
+                                if (byte == FLAG) {
+                                    state = STOP;
+                                } else {
+                                    state = START;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                // If we haven't received the wished frames, decrement retransmissions
+                retransmissions--;
+            }
+            if (state != STOP) {
+                printf("ERROR: Failed to establish connection\n");
+                return -1;
+            }
+            break;
+        case LlRx:
+            while (state != STOP) {
+                // Read byte from serial port
+                unsigned char byte;
+                if (readByteSerialPort(&byte) < 0) {
+                    printf("ERROR: Failed to read byte from serial port\n");
+                    return -1;
+                }
+                else {
+                    switch (state) {
+                        case START:
+                            if (byte == FLAG) {
+                                state = FLAG_RCV;
+                            }
+                            break;
+                        case FLAG_RCV:
+                            if (byte == A_TX) {
+                                state = A_RCV;
+                            } else if (byte != FLAG) {
+                                state = START;
+                            }
+                            break;
+                        case A_RCV:
+                            if (byte == C_SET) {
+                                state = C_RCV;
+                            } else if (byte == FLAG) {
+                                state = FLAG_RCV;
+                            } else {
+                                state = START;
+                            }
+                            break;
+                        case C_RCV:
+                            if (byte == (A_TX^C_SET)) {
+                                state = BCC_OK;
+                            } else if (byte == FLAG) {
+                                state = FLAG_RCV;
+                            } else {
+                                state = START;
+                            }
+                            break;
+                        case BCC_OK:
+                            if (byte == FLAG) {
+                                state = STOP;
+                            } else {
+                                state = START;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            // Write control frame
+            if (writeBytesSerialPort(controlFrameRX, 5) < 0) {
+                printf("ERROR: Failed to write control frame\n");
+                return -1;
+            }
+            break;
+        default:
+            return -1;
+    }
+    
+    return fd;
 }
 
 ////////////////////////////////////////////////
 // LLWRITE
 ////////////////////////////////////////////////
-int llwrite(const unsigned char *buf, int bufSize)
-{
-    // TODO
+int llwrite(const unsigned char *buf, int bufSize) {
+    // Frame format: FLAG A C BCC1 D1 D2 ... DN BCC2 FLAG
+    int frameSize = bufSize + 6;
+    unsigned char *frame = (unsigned char *) malloc(frameSize);
+    frame[0] = FLAG;
+    frame[1] = A_TX;
+    frame[2] = trama << 6;
+    frame[3] = A_TX^0x00;
+    memcpy(frame+4, buf, bufSize);
 
-    return 0;
+    // Calculate BCC2
+    unsigned char BCC2 = buf[0];
+    for (int i = 1; i < bufSize; i++) {
+        BCC2 ^= buf[i];
+    }
+
+    // Stuffing
+    int j = 4;
+    for (int i = 0; i < bufSize; i++) {
+        if (buf[i] == FLAG || buf[i] == ESCAPE) {
+            frame[j++] = ESCAPE;
+            frame[j++] = buf[i]^STUFF;
+        } else {
+            frame[j++] = buf[i];
+        }
+    }
+    frame[j++] = BCC2;
+    frame[j++] = FLAG;
+
+    // Send frame
+    int current = 0;
+    int accepted = FALSE;
+    while (current < retransmissions && !accepted) {
+        alarm(timeout);
+        alarmTriggered = FALSE;
+        accepted = FALSE;
+
+        // While we haven't received the wished frames and the alarm hasn't been triggered
+        while (!alarmTriggered && !accepted) {
+            // Write frame
+            if (writeBytesSerialPort(frame, frameSize) < 0) {
+            printf("ERROR: Failed to write frame\n");
+            return -1;
+            }
+            // Read byte from serial port
+            unsigned char byte;
+            if (readByteSerialPort(&byte) < 0) {
+                printf("ERROR: Failed to read byte from serial port\n");
+                return -1;
+            }
+            // Check result
+            else {
+                switch (byte) {
+                    case C_RR(0): case C_RR(1):
+                        accepted = TRUE;
+                        break;
+                    case C_REJ(0): case C_REJ(1):
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        if (!accepted) current++;
+        else break;
+    }
+
+    if (accepted) return bufSize;
+    return -1;
 }
 
 ////////////////////////////////////////////////
@@ -48,17 +277,13 @@ int llwrite(const unsigned char *buf, int bufSize)
 int llread(unsigned char *packet)
 {
     // TODO
-
     return 0;
 }
 
 ////////////////////////////////////////////////
 // LLCLOSE
 ////////////////////////////////////////////////
-int llclose(int showStatistics)
-{
-    // TODO
-
+int llclose(int showStatistics) {
     int clstat = closeSerialPort();
     return clstat;
 }
